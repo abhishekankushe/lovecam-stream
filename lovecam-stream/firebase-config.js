ef33c1557993ef6737fc51802cd1286aafc07836
abhishek Ankushe <abhishekankushe3@gmail.com>
2025-07-12 23:36:03 +0530

    ✅ Fixed Firebase config

--- lovecam-stream/firebase-config.js
+++ lovecam-stream/firebase-config.js
@@ -3,9 +3,8 @@ export const firebaseConfig = {
   authDomain: "realtime-database-3e08c.firebaseapp.com",
   databaseURL: "https://realtime-database-3e08c-default-rtdb.firebaseio.com",
   projectId: "realtime-database-3e08c",
-  storageBucket: "realtime-database-3e08c.firebasestorage.app",
+  storageBucket: "realtime-database-3e08c.appspot.com",
   messagingSenderId: "492020386670",
   appId: "1:492020386670:web:46df7c0ecf9ecbfdc90f7f",
   measurementId: "G-KWC5D1G5ME"
 };
-249
